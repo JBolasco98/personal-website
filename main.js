aed6d29c4f79276beba17cb98f75769c1e180b21
Jomar <jomarbolasco98.com>
2024-09-12 14:12:20 +0800

    finalasizing

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ const navLinks = document.querySelectorAll('.nav-link');
 navLinks.forEach(link => {
   link.addEventListener('click', (e) => {
     e.preventDefault();
-    const targetSectionId = link.getAttribute('href').substring(1); // Extract ID from href
+    const targetSectionId = link.getAttribute('href').substring(1);
 
-    // Hide all sections
+
     document.querySelectorAll('section').forEach(section => {
       section.style.display = 'none';
     });
 
-    // Show the target section
+
     document.getElementById(targetSectionId).style.display = 'block';
   });
 });
